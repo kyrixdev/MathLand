dc41922a1e4e0991c2029e38ac795cc177dabc08
Oussama Kefi <kefioussama595@gmail.com>
2023-01-17 22:25:13 +0000

    update facebook link

--- controllers/footer.php
+++ controllers/footer.php
@@ -2,7 +2,7 @@
 <p>MATHLAND 2023. Géré par : Nasraoui Tarek        </p>
         <a href="https://clickandtick.com" class="line-1 anim-typewriter">Click & Tick Agency <i class="fas fa-code text-white" aria-hidden="true"></i> with <i class="fas fa-heart text-red-500" aria-hidden="true"></i>. </a>
         <br>
-        <a href="#facebook"><i class="fab fa-facebook-square text-3xl"></i></a>
+        <a href="https://www.facebook.com/mathlandapp"><i class="fab fa-facebook-square text-3xl"></i></a>
         <a href="#youtube"><i class="fab fa-youtube text-3xl"></i></a>
 </footer>
 </body>
